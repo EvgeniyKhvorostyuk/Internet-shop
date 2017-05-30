180d459ab1e12fac0a8a7301d53f46b68411b4db
Maverik <evgeniykhvorostyuk@gmail.com>
2017-05-30 15:01:08 +0300

    database connection

---
+++ config/db_params.php
@@ -0,0 +1,8 @@
+<?php
+
+return array(
+    'host' => 'localhost',
+    'dbname' => 'Ishop_db',
+    'user' => 'root',
+    'password' => '7315',            
+);
--- index.php
+++ index.php
@@ -1,4 +1,13 @@
 <?php
 
-echo "hello";
+//Front controller
 
+ini_set('display_errors', 1);
+error_reporting(E_ALL );
+
+define('ROOT', dirname(__FILE__));
+require_once(ROOT.'/components/Router.php');
+require_once(ROOT.'/components/Db.php'); 
+
+$router = new Router();
+$router->run();   
