bf6c705d6db191f325bdc43835077eeeec6f747f
Maverik <evgeniykhvorostyuk@gmail.com>
2017-06-05 16:25:27 +0300

    output of the single product page

--- controllers/ProductController.php
+++ controllers/ProductController.php
@@ -1,10 +1,21 @@
 <?php
 
+require_once ROOT . '/models/Category.php';
+require_once ROOT . '/models/Product.php';
+
 class ProductController
 {
 
-	public function actionView($id)
+	public function actionView($productId)
 	{
+
+		// Список категорий для левого меню
+		$categories = array();
+        $categories = Category::getCategoriesList();
+        
+        // Получаем инфомрацию о товаре
+        $product = Product::getProductById($productId);
+		
 		require_once(ROOT.'/views/product/view.php');
 
 		return true;
--- models/Product.php
+++ models/Product.php
@@ -7,12 +7,6 @@ class Product
 	// Количество отображаемых товаров по умолчанию
     const SHOW_BY_DEFAULT = 6;
 
-    /**
-     * Возвращает массив последних товаров
-     * @param type $count [optional] <p>Количество</p>
-     * @param type $page [optional] <p>Номер текущей страницы</p>
-     * @return array <p>Массив с товарами</p>
-     */
     public static function getLatestProducts($count = self::SHOW_BY_DEFAULT)
     {
         
@@ -48,4 +42,86 @@ class Product
         }
         return $productsList;
     }
+
+    public static function getProductsListByCategory($categoryId, $page = 1)
+    {
+        $limit = Product::SHOW_BY_DEFAULT;
+        
+        // Смещение (для запроса)
+        $offset = ($page - 1) * self::SHOW_BY_DEFAULT;
+        
+        // Соединение с БД
+        $db = Db::getConnection();
+        
+        // Текст запроса к БД
+        $sql = 'SELECT id, name, price, is_new FROM product WHERE status = 1 AND category_id = :category_id ORDER BY id ASC LIMIT :limit OFFSET :offset';
+        
+        // Используется подготовленный запрос
+        $result = $db->prepare($sql);
+        $result->bindParam(':category_id', $categoryId, PDO::PARAM_INT);
+        $result->bindParam(':limit', $limit, PDO::PARAM_INT);
+        $result->bindParam(':offset', $offset, PDO::PARAM_INT);
+        
+        // Выполнение коменды
+        $result->execute();
+        
+        // Получение и возврат результатов
+        $i = 0;
+        $products = array();
+        
+        while ($row = $result->fetch()) {
+            $products[$i]['id'] = $row['id'];
+            $products[$i]['name'] = $row['name'];
+            $products[$i]['price'] = $row['price'];
+            $products[$i]['is_new'] = $row['is_new'];
+            $i++;
+        
+        }
+        return $products;
+    }
+
+    public static function getProductById($id)
+    {
+        // Соединение с БД
+        $db = Db::getConnection();
+        
+        // Текст запроса к БД
+        $sql = 'SELECT * FROM product WHERE id = :id';
+       
+        // Используется подготовленный запрос
+        $result = $db->prepare($sql);
+        $result->bindParam(':id', $id, PDO::PARAM_INT);
+        
+        // Указываем, что хотим получить данные в виде массива
+        $result->setFetchMode(PDO::FETCH_ASSOC);
+        
+        // Выполнение коменды
+        $result->execute();
+        
+        // Получение и возврат результатов
+        return $result->fetch();
+    }
+
+    public static function getTotalProductsInCategory($categoryId)
+    {
+        // Соединение с БД
+        $db = Db::getConnection();
+        
+        // Текст запроса к БД
+        $sql = 'SELECT count(id) AS count FROM product WHERE status="1" AND category_id = :category_id';
+        
+        // Используется подготовленный запрос
+        $result = $db->prepare($sql);
+        $result->bindParam(':category_id', $categoryId, PDO::PARAM_INT);
+        
+        // Выполнение коменды
+        $result->execute();
+        
+        // Возвращаем значение count - количество
+        $row = $result->fetch();
+        
+        return $row['count'];
+    }
+
+
 }
