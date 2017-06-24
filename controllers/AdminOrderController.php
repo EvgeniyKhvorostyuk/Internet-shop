d62be297f2e5f566d4f295f52c0a5843732c8212
Maverik <evgeniykhvorostyuk@gmail.com>
2017-06-24 14:40:31 +0300

    adminpanel: editing and deleting order functions

--- controllers/AdminOrderController.php
+++ controllers/AdminOrderController.php
@@ -29,5 +29,48 @@ class AdminOrderController extends AdminBase
 		require_once(ROOT . '/views/admin_order/view.php');
 
 		return true;
-	}	
+	}
+
+	public function actionUpdate($id)
+    {
+        
+        self::checkAdmin();
+        
+        $order = Order::getOrderById($id);
+       
+        if (isset($_POST['submit'])) {
+            
+            $userName = $_POST['userName'];
+            $userPhone = $_POST['userPhone'];
+            $userComment = $_POST['userComment'];
+            $date = $_POST['date'];
+            $status = $_POST['status'];
+            
+            // Сохраняем изменения
+            Order::updateOrderById($id, $userName, $userPhone, $userComment, $date, $status);
+            
+
+            header("Location: /admin/order/view/$id");
+        }
+        
+        require_once(ROOT . '/views/admin_order/update.php');
+        return true;
+    }
+
+    public function actionDelete($id)
+    {
+        
+        self::checkAdmin();
+        
+        if (isset($_POST['submit'])) {
+            
+            Order::deleteOrderById($id);
+            
+            header("Location: /admin/order");
+        }
+        
+        require_once(ROOT . '/views/admin_order/delete.php');
+        
+        return true;
+    }	
 }
